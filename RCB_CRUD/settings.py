9fc267bfa7288268756c0a104049f63b2c033d6e
Prabhakar617 <prabasvu@gmail.com>
2025-03-21 17:36:39 +0530

    prod static folder name updated

--- RCB_CRUD/settings.py
+++ RCB_CRUD/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'project_static',]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "prodstatic")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
